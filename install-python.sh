ff49a1cf73b0f8fc6bd799cebdd98633278d179c
Pieter Pas <pieter.pas@outlook.com>
2023-01-17 14:25:10 +0100

    Upgrade Python versions

--- install-python.sh
+++ install-python.sh
@@ -11,16 +11,15 @@ function build_cpython {
     # opts="--with-lto"
     case $version in
         3.7)
-            full_version="3.7.15";;
+            full_version="3.7.16";;
         3.8)
-            full_version="3.8.15";;
+            full_version="3.8.16";;
         3.9)
-            full_version="3.9.15";;
+            full_version="3.9.16";;
         3.10)
-            full_version="3.10.8";;
+            full_version="3.10.9";;
         3.11)
-            full_version="3.11.0"
-            version_suffix="rc2";;
+            full_version="3.11.1";;
     esac
     python="Python-${full_version}${version_suffix}"
 
@@ -52,9 +51,9 @@ function install_pypy {
         pypy3.7)
             full_version="7.3.9";;
         pypy3.8)
-            full_version="7.3.9";;
+            full_version="7.3.11";;
         pypy3.9)
-            full_version="7.3.9";;
+            full_version="7.3.11";;
     esac
     mkdir -p "$prefix"
     wget -O- "https://downloads.python.org/pypy/$version-v$full_version-linux64.tar.bz2" | tar -xj --strip-components=1 -C "$prefix"
